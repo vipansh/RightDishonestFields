dda436f3cb0bf16fdeea28774c5ad44267a6b379
Ishika <116502498+Ishika54217@users.noreply.github.com>
2024-10-28 17:27:58 +0000

    Fixed Nivigation Bar for Mobile

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,12 @@
 import { Outlet } from "react-router-dom";
-import Header from "./Header";
+import Navbar from "./Navbar";
 import Footer from "./Footer";
 
 const Layout = () => {
   return (
     <div className="min-h-screen flex flex-col">
-      <Header />
-      <main className="flex-1">
+      <Navbar />
+      <main className="flex-1 pt-12">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <Outlet />
         </div>
